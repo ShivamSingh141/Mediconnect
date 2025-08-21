a8a2abe71255f41913e36b854fd9c1577c4c3b74
Shivam singh <shivamsingh1414j@gmail.com>
2025-08-22 01:22:12 +0530

    initial commit

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+import { doctors } from "../assets/assets";
+
+export const AppContext=createContext()
+
+const AppContextprovider=(props)=>{
+   const value={
+   doctors
+    }
+    return(
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+
+}
+
+export default AppContextprovider
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+colors:{
+  'primary':"#3B82F6"
+},
+gridTemplateColumns:{
+  'auto':'repeat(auto-fill,minmax(200px,1fr))'
+}
+
+    },
+  },
+  plugins: [],
+}
+
